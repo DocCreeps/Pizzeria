62a9203d5b9d0faad5f808ddc6c29e37b3de5531
Dorian VERICEL <dorian1998dino@hotmail.com>
2021-12-08 14:35:46 +0100

    init

---
+++ Form1.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace Pizzeria
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.comboClient = new System.Windows.Forms.ComboBox();
+            this.dataPizza = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataPizza)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // comboClient
+            // 
+            this.comboClient.FormattingEnabled = true;
+            this.comboClient.Location = new System.Drawing.Point(339, 46);
+            this.comboClient.Name = "comboClient";
+            this.comboClient.Size = new System.Drawing.Size(339, 21);
+            this.comboClient.TabIndex = 0;
+            // 
+            // dataPizza
+            // 
+            this.dataPizza.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataPizza.Location = new System.Drawing.Point(267, 164);
+            this.dataPizza.Name = "dataPizza";
+            this.dataPizza.Size = new System.Drawing.Size(391, 150);
+            this.dataPizza.TabIndex = 1;
+            this.dataPizza.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataPizza_CellContentClick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dataPizza);
+            this.Controls.Add(this.comboClient);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataPizza)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboClient;
+        private System.Windows.Forms.DataGridView dataPizza;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Pizzeria
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        masterEntities db = new masterEntities();
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            dataPizza.DataSource = db.CLIENT.ToList();
+
+
+            List<CLIENT> ListC = new List<CLIENT>();
+            ListC.AddRange(db.CLIENT.ToList());
+            comboClient.ValueMember = "NumClient";
+            comboClient.DisplayMember = "NomClient";
+            comboClient.DataSource = ListC;
+        }
+
+    }
+}
